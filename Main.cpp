4b9b2942f380f374f9b22743cd667cdf966035eb
Jose-Salazar-30 <jsalazarm10@miumg.edu.gt>
2024-02-18 12:31:14 -0600

    Creacion Clase Main

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include "Estudiante.cpp"
+#include <iostream>
+using namespace std;
+
+main() {
+	//Instancia de un objeto
+	Estudiante estudiante = Estudiante("Jose","Salazar","Guatemala",12345678,"2024-02-18","7691");
+	
+	estudiante.leer();
+}
